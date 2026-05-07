7b34efaaf385638829c5c3f813b1be8c0e5703c1
Neil-023 <neiljohn.nj23@gmail.com>
2026-05-07 17:53:09 +0800

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -21,13 +21,13 @@ export default defineConfig({
         scope: "/",
         icons: [
           {
-            src: "/logo.png",
+            src: "logo.png",
             sizes: "192x192",
             type: "image/png",
             purpose: "any"
           },
           {
-            src: "/logo.png",
+            src: "logo.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any maskable"
